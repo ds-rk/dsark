dc8b896aade5ed82d6bb846a7ffdcacfc7fa22a7
sourav <souravbera472@gmail.com>
2023-05-14 00:20:45 +0530

    Initial commit

---
+++ src/main/java/com/dsark/DsarkApplication.java
@@ -0,0 +1,13 @@
+package com.dsark;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsarkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsarkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dsark/DsarkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dsark;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsarkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
